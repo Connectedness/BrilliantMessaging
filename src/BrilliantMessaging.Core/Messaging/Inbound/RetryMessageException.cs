7779851056b0d6d8d40911a6a894ac1063c81a4a
Kenny Pflug <kenny.pflug@live.de>
2026-06-26 21:25:47 +0200

    refactor: simplify RetryMessageException
    Signed-off-by: Kenny Pflug <kenny.pflug@live.de>

--- src/BrilliantMessaging.Core/Messaging/Inbound/RetryMessageException.cs
+++ src/BrilliantMessaging.Core/Messaging/Inbound/RetryMessageException.cs
@@ -9,24 +9,14 @@ namespace BrilliantMessaging.Core.Messaging.Inbound;
 public sealed class RetryMessageException : Exception
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="RetryMessageException" /> class.
-    /// </summary>
-    public RetryMessageException()
-        : base("The inbound message was explicitly marked retryable.") { }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="RetryMessageException" /> class with a custom message.
-    /// </summary>
-    /// <param name="message">The exception message.</param>
-    public RetryMessageException(string message)
-        : base(message) { }
-
-    /// <summary>
     /// Initializes a new instance of the <see cref="RetryMessageException" /> class with a custom message and
     /// inner exception.
     /// </summary>
     /// <param name="message">The exception message.</param>
     /// <param name="innerException">The exception that caused this retry marker.</param>
-    public RetryMessageException(string message, Exception innerException)
+    public RetryMessageException(
+        string message = "The inbound message was explicitly marked retryable.",
+        Exception? innerException = null
+    )
         : base(message, innerException) { }
 }
